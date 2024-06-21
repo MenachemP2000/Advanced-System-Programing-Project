9cd28a6d409e306b8f2c889353f5a7ba81086f61
Menachem Parente <menachem.parente@live.biu.ac.il>
2024-06-21 16:22:03 +0300

    move around some files

--- Server/server.js
+++ Server/server.js
@@ -42,15 +42,15 @@ app.use('/api/users', userRoutes);
 app.use('/api/videos', videoRoutes);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../web-devlopment-build/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../web-devlopment-build/build', 'index.html'));
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on localhost:${port}`);
 });
---
+++ web-devlopment-build/src/App/config.js
@@ -0,0 +1,5 @@
+const config = {
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://192.168.68.101:4000',
+};
+
+export default config;
