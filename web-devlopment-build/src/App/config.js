12785e71960d9ebf8daefe678145c49681c9fffb
Menachem Parente <menachem.parente@live.biu.ac.il>
2024-06-24 07:45:48 +0300

    fixed public folder gets deleted after "run build"....

--- web-devlopment-build/src/App/config.js
+++ web-devlopment-build/src/App/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://192.168.68.101:4000',
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://10.30.114.167:4000',
 };
 
 export default config;
